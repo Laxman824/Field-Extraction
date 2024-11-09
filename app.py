f331ac6367bde935ec6e3d6de37aea7bdcd09bd7
K Laxman <71651991+Laxman824@users.noreply.github.com>
2024-11-09 15:42:31 +0530

    12

--- app.py
+++ app.py
@@ -286,6 +286,7 @@ def main():
 
     # Create sidebar and get selected options
     selected_fields, confidence_threshold = create_sidebar()
+    import os
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     sys.path.append(project_root)
     
